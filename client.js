d8c73a0cf149054c571ce72aa9b5fcb1cef75cd8
ichtrojan <ordrizzy@gmail.com>
2020-06-15 14:49:23 +0100

    update to fit logrocket article

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const GRPCClient = require('node-grpc-client')
 
 const path = require("path")
 
-const PROTO_PATH = path.resolve(__dirname, './proto/chat.proto')
+const PROTO_PATH = path.resolve(__dirname, 'chat.proto')
 
 dotenv.config()
 
@@ -14,9 +14,18 @@ const HOST = process.env.RPC_HOST
 const client = new GRPCClient(PROTO_PATH, 'chat', 'ChatService', `${HOST}:${PORT}`)
 
 const message = {
-    body: "Hello from NodeJS"
+    body: "Hello from Trojan",
+}
+
+const details = {
+    name: "Trojan",
+    age: 40
 }
 
 client.runService('SayHello', message, (err, res) => {
-    console.log(res);
+    console.log(res.body);
+});
+
+client.runService('GetDetails', details, (err, res) => {
+    console.log(res.body);
 });
